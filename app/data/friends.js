d8b832ccb037a9a5d3b98b81b95cfed39266a49d
Max Yao <bojinyao@berkeley.edu>
2018-08-17 01:12:44 -0700

    Added photos and some placeholder people.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 var friends = [
 
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
             5,
             1,
             4,
@@ -14,8 +14,53 @@ var friends = [
             5,
             4,
             1
-          ]
-      }
+        ]
+    }, {
+        "name": "Nancey",
+        "photo": "../images/Nancey.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    }, {
+        "name": "Stephany",
+        "photo": "../images/Stephany.jpg",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    }, {
+        "name": "James",
+        "photo": "../images/James.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    }
 ];
 
 module.exports = friends;
