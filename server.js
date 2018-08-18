0b5328b210455a29134002755c2fd529849edf67
Max Yao <bojinyao@berkeley.edu>
2018-08-18 12:38:27 -0700

    making images accessible

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('images'));
 
 const PORT = process.env.PORT || 8080;
 
